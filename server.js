c93ca9fd07e9aa66bd9cc944e9bd5f875fb55d37
Ben Nguessan <nguessanben31@gmail.com>
2026-04-01 01:22:31 +0000

    mise a jour par ben

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.use((req, res) => {
 });
 
 // ---------------- SERVER ----------------
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
